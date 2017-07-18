10682c85abc55af9c777f9249c2ebd5661fcff33
Alcides-Carlos <alcides.carlos@datakron.com>
2017-07-18 21:50:34 +0000

    arquivos desnecessários

--- src/pegar_parciais.R
+++
@@ -1,7 +0,0 @@
-dados_parciais <- fromJSON( 
-  paste( 
-    readLines( "https://api.cartolafc.globo.com/atletas/pontuados" ), 
-    collapse = "" )
-)
-
-#"https://api.cartolafc.globo.com/atletas/pontuados"
