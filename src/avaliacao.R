fc9840d07347421f62e0d3c4d5622b084c7028a2
Alcides-Carlos <alcides.carlos@datakron.com>
2017-07-19 18:50:56 +0000

    Script para avaliar resultados das escalações sugeridas.

--- src/avaliacao.R
+++ src/avaliacao.R
@@ -1,6 +1,14 @@
+#############################################################################
+###################### Carrega Resultados das Análises ######################
+#############################################################################
+
 load( "reports/resultados_times.RData" )
 
-# Avaliacao dos resultados
+######################################################################
+###################### Avaliacao dos resultados ######################
+######################################################################
+
+# Resultados gerais
 
 resultados_times %>% 
   group_by( escalacao ) %>% 
@@ -11,14 +19,21 @@ resultados_times %>%
   arrange( desc(pontuacao_real) )
 
 
+# Resultados por escalação e rodada
+
 resultados_times %>% 
   arrange( escalacao, rodada )
+
+
+# Resultados por escalação específica
 
 resultados_times %>% 
   distinct( escalacao )
 
 resultados_times %>% 
   filter( escalacao == "esc_4_3_3" )
+
+# Resultados por rodada específica
 
 resultados_times %>% 
   distinct( rodada )
@@ -28,13 +43,7 @@ resultados_times %>%
   arrange( desc(pontuacao_real) )
 
 
-resultados_times %>% 
-  ggplot( aes( x = rodada, 
-               y = pontuacao_real, 
-               group = escalacao, 
-               colour = escalacao ) ) +
-  geom_line()
-
+# Visão pontuação esperada
 
 resultados_times %>% 
   ggplot( aes( x = rodada, 
@@ -43,6 +52,17 @@ resultados_times %>%
                colour = escalacao ) ) +
   geom_line()
 
+# Visão pontuação esperada
+
+resultados_times %>% 
+  ggplot( aes( x = rodada, 
+               y = pontuacao_real, 
+               group = escalacao, 
+               colour = escalacao ) ) +
+  geom_line()
+
+
+# Visão pontuação esperada x real 3-4-3
 
 resultados_times %>% 
   filter( escalacao == "esc_3_4_3" ) %>% 
@@ -51,6 +71,9 @@ resultados_times %>%
   geom_line( aes( y = pontuacao_real, colour = "pontuacao_real" ) ) +
   ylab("pontuação")
 
+
+# Visão pontuação esperada x real 3-5-2
+
 resultados_times %>% 
   filter( escalacao == "esc_3_5_2" ) %>% 
   ggplot( aes( x = rodada ) ) +
@@ -58,11 +81,17 @@ resultados_times %>%
   geom_line( aes( y = pontuacao_real, colour = "pontuacao_real" ) ) +
   ylab("pontuação")
 
+
+# Visão pontuação esperada x real 4-3-3
+
 bind_rows( resultados_times ) %>% 
   filter( escalacao == "esc_4_3_3" ) %>% 
   ggplot( aes( x = rodada ) ) +
   geom_line( aes( y = pontuacao_esperada, colour = "pontuacao_esperada" ) ) +
   geom_line( aes( y = pontuacao_real, colour = "pontuacao_real" ) )
+
+
+# Visão pontuação esperada x real 4-4-2
 
 resultados_times %>% 
   filter( escalacao == "esc_4_4_2" ) %>% 
@@ -71,6 +100,9 @@ resultados_times %>%
   geom_line( aes( y = pontuacao_real, colour = "pontuacao_real" ) ) +
   ylab("pontuação")
 
+
+# Visão pontuação esperada x real 4-5-1
+
 resultados_times %>% 
   filter( escalacao == "esc_4_5_1" ) %>% 
   ggplot( aes( x = rodada ) ) +
@@ -78,12 +110,16 @@ resultados_times %>%
   geom_line( aes( y = pontuacao_real, colour = "pontuacao_real" ) ) +
   ylab("pontuação")
 
+# Visão pontuação esperada x real 5-3-2
+
 resultados_times %>% 
   filter( escalacao == "esc_5_3_2" ) %>% 
   ggplot( aes( x = rodada ) ) +
   geom_line( aes( y = pontuacao_esperada, colour = "pontuacao_esperada" ) ) +
   geom_line( aes( y = pontuacao_real, colour = "pontuacao_real" ) ) +
   ylab("pontuação")
+
+# Visão pontuação esperada x real 5-4-1
 
 resultados_times %>% 
   filter( escalacao == "esc_5_4_1" ) %>% 
