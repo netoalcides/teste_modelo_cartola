0db00093e3ce24f893d9367d1a3fcb59be7bf8e7
Alcides-Carlos <alcides.carlos@datakron.com>
2017-07-19 19:14:47 +0000

    Ajusta dados (nomes, scouts)

--- munge/04 - AjustaDados.R
+++ munge/04 - AjustaDados.R
@@ -1,23 +1,28 @@
-info( logger, "CARTOLA_2017::ajusta dados do dataset" )
+if ( atualiza == 1 ) {
+  
+  info( logger, "CARTOLA_2017::ajusta dados do dataset" )
+  
+  dados_cartola_2017_por_rodada %<>%
+    arrange( atleta_id, rodada_id ) %>%
+    select( atleta_id, FC, PE, RB, SG, CA, FD, FS, FF, I, G, DD, GS, A, CV, FT, GC, DP, PP ) %>%
+    group_by( atleta_id ) %>%
+    mutate_all( funs( ajuste_lag(.) ) ) %>%
+    bind_cols( dados_cartola_2017_por_rodada %>%
+                 arrange( atleta_id, rodada_id ) %>%
+                 select( -atleta_id, -FC, -PE, -RB, -SG, -CA, -FD, -FS, -FF, -I, -G, -DD, -GS,
+                         -A, -CV, -FT, -GC, -DP, -PP ), .) %>%
+    mutate( apelido = replace( apelido, apelido == "Patrick", "Patrick Correia" ) ) %>%
+    mutate( apelido = replace( apelido, apelido == "Vinicius Júnior", "Vinicius Junior" ) ) %>%
+    mutate( apelido = replace( apelido, apelido == "Matheus Sávio", "Matheus Savio" ) ) %>%
+    mutate( apelido = replace( apelido, apelido == "Mateus Pet", "Mateus Vital" ) ) %>%
+    data.frame()
+  
+  
+  info( logger, "CARTOLA_2017::cache informacoes dos dados por rodada" )
+  
+  cache( "dados_cartola_2017_por_rodada" )
+  
+  rm(dados_cartola_2017_por_rodada)
+  
+}
 
-dados_cartola_2017_por_rodada %<>%
-  arrange( atleta_id, rodada_id ) %>%
-  select( atleta_id, FC, PE, RB, SG, CA, FD, FS, FF, I, G, DD, GS, A, CV, FT, GC, DP, PP ) %>%
-  group_by( atleta_id ) %>%
-  mutate_all( funs( ajuste_lag(.) ) ) %>%
-  bind_cols( dados_cartola_2017_por_rodada %>%
-               arrange( atleta_id, rodada_id ) %>%
-               select( -atleta_id, -FC, -PE, -RB, -SG, -CA, -FD, -FS, -FF, -I, -G, -DD, -GS,
-                       -A, -CV, -FT, -GC, -DP, -PP ), .) %>%
-  mutate( apelido = replace( apelido, apelido == "Patrick", "Patrick Correia" ) ) %>%
-  mutate( apelido = replace( apelido, apelido == "Vinicius Júnior", "Vinicius Junior" ) ) %>%
-  mutate( apelido = replace( apelido, apelido == "Matheus Sávio", "Matheus Savio" ) ) %>%
-  mutate( apelido = replace( apelido, apelido == "Mateus Pet", "Mateus Vital" ) ) %>%
-  data.frame()
-
-
-info( logger, "CARTOLA_2017::cache informacoes dos dados por rodada" )
-
-cache( "dados_cartola_2017_por_rodada" )
-
-rm(dados_cartola_2017_por_rodada)
